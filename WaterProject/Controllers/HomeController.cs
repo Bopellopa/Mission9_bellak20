9dbe277bece60a1d7e5d3dab21e76320c11b7676
bopellopa <bellak20@byu.edu>
2023-03-13 23:30:44 -0600

    new

--- WaterProject/Controllers/HomeController.cs
+++ WaterProject/Controllers/HomeController.cs
@@ -3,14 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WaterProject.Models;
+using WaterProject.Models.ViewModels;
 
 namespace WaterProject.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private IBookstoreRepository repo;
+        public HomeController(IBookstoreRepository temp)
         {
-            return View();
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.BookId)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+                PageInfo = new PageInfo
+                {
+                    TotalNumProjects = repo.Books.Count(),
+                    ProjectsPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+           
+            return View(x);
         }
     }
 }
